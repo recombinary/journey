2d283c890a7c727550871a55a40a164e35cf354f
Dan Davey <dan@recombinary.com>
2015-12-13 19:04:01 +0200

    fixes denormalization

--- lib/journey/resource/attribute_loading.rb
+++ lib/journey/resource/attribute_loading.rb
@@ -7,9 +7,18 @@ module Journey::Resource::AttributeLoading
     def load(attributes, remove_root = false, persisted = false)
       # 'data' is a reserved key in ActiveResource,
       # but Journey uses it for the Oplog
-      attributes['content'] = attributes.delete('data')
+      if content = attributes.delete('data')
+        attributes['content'] = content
+      end
 
       super(attributes, remove_root, persisted).tap do
+
+        # set the cached value of any updated associations (e.g. parent_id)
+        # to nil so that they can be re-fetched
+        attributes.keys.map(&:to_s).select{ |key| key =~ /_id$/ }.each do |association_key|
+          association = association_key.gsub /_id$/, ''
+          instance_variable_set("@#{association}", nil)
+        end
 
         # allow enum_sets to be loaded by key (rather than index)
         # by auto-converting them on initialization
